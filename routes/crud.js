09a366056275eb6f5410eb883e8e4efd97f8d428
Tarun Sharma <44375597+tarunshrm786@users.noreply.github.com>
2023-11-09 19:17:00 +0530

    Update crud.js

--- routes/crud.js
+++ routes/crud.js
@@ -13,42 +13,62 @@ const Book = mongoose.model('Book', bookSchema);
 
 // Create a new book
 router.post('/', async (req, res) => {
-  const { title, author, summary } = req.body;
-  const book = new Book({ title, author, summary });
-  await book.save();
-  res.status(201).json(book);
+  try {
+    const { title, author, summary } = req.body;
+    const book = new Book({ title, author, summary });
+    await book.save();
+    res.status(201).json(book);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to create a new book' });
+  }
 });
 
 // Get a list of all books
 router.get('/', async (req, res) => {
-  const books = await Book.find();
-  res.json(books);
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve books' });
+  }
 });
 
 // Get details of a specific book by its ID
 router.get('/:id', async (req, res) => {
-  const book = await Book.findById(req.params.id);
-  if (!book) return res.status(404).json({ message: 'Book not found' });
-  res.json(book);
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    res.json(book);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve the book' });
+  }
 });
 
 // Update a book's details
 router.put('/:id', async (req, res) => {
-  const { title, author, summary } = req.body;
-  const book = await Book.findByIdAndUpdate(
-    req.params.id,
-    { title, author, summary },
-    { new: true }
-  );
-  if (!book) return res.status(404).json({ message: 'Book not found' });
-  res.json(book);
+  try {
+    const { title, author, summary } = req.body;
+    const book = await Book.findByIdAndUpdate(
+      req.params.id,
+      { title, author, summary },
+      { new: true }
+    );
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    res.json(book);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to update the book' });
+  }
 });
 
 // Delete a book
 router.delete('/:id', async (req, res) => {
-  const book = await Book.findOneAndDelete({ _id: req.params.id });
-  if (!book) return res.status(404).json({ message: 'Book not found' });
-  res.json({ message: 'Book deleted' });
+  try {
+    const book = await Book.findOneAndDelete({ _id: req.params.id });
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    res.json({ message: 'Book deleted' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete the book' });
+  }
 });
 
 module.exports = router;
